85a6503845d6ce323190f27283f8abaa11e9524e
ArunTeltia <teltia.arun18@gmail.com>
2020-07-23 22:27:29 +0530

    socket orbs

--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,3 +1,17 @@
 const io = require('../server').io
+const Orb = require('./classes/Orb');
+let orbs=[]
+initGame();
 
+io.sockets.on('connect',(socket)=>{
+    socket.emit('init',{
+        orbs
+    })
+})
+
+function initGame(){
+    for(let i=0;i<500;i++){
+        orbs.push(new Orb())
+    }
+}
 module.exports = io
