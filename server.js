0a074e878784bc364bec2c1c69ef9bd34e40f917
ArunTeltia <teltia.arun18@gmail.com>
2020-07-23 14:09:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+const socketio = require('socket.io');
+const expressServer = app.listen(8080);
+const io = socketio(expressServer);
+const helmet = require('helmet')
+app.use(helmet());
+console.log("express and socketio server listening on port 8080")
+
+//App orgnisation
+module.exports = {
+    app, io
+}
---
+++ sockets/socketMain.js
@@ -0,0 +1,3 @@
+const io = require('../server').io
+
+module.exports = io
